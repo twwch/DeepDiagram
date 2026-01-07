7a425850219c8fceef92d90195c53855a4c52d91
陈毫 <2329095893@qq.com>
2026-01-07 13:44:34 +0800

    fix: build error

--- frontend/dist/assets/stateDiagram-v2-4FDKWEC3-BkMW-sr3.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-DRkv9aMW.js";import{_ as i}from"./index-GLMV_w7a.js";import"./chunk-55IACEB6-CqsdIG1c.js";import"./chunk-QN33PNHL-Dygbv7ne.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
